eb68c70b79538ed680e12999902d6aafa122c9f1
Tekai_ <vip007_007@mail.ru>
2023-07-05 00:01:24 +0300

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+import { Box, Text, Button } from "@chakra-ui/react";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
+        <p>Header</p>
       </header>
+      <Box
+        bg="tomato"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <ButtonsFolder />
+      </Box>
     </div>
   );
 }
 
 export default App;
+
+function ButtonsFolder() {
+  const field = Array(10)
+    .fill(null)
+    .map((_, i) => {
+      return (
+        <Button key={i} w="40px" height="40px" margin="4px">
+          {i}
+        </Button>
+      );
+    });
+  return (
+    <Box display="flex" justifyContent="row" w="200px" flexWrap="wrap">
+      {field}
+    </Box>
+  );
+}
