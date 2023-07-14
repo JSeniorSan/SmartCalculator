bb5aabe9ea8ba16a9a479e6d1d98d059fb9aeb62
Tekai_ <vip007_007@mail.ru>
2023-07-15 01:21:20 +0300

    Menu step1.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import { Box, Text, Button, Input, Flex } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  Button,
+  Input,
+  Flex,
+  ListItem,
+  UnorderedList,
+  List,
+} from "@chakra-ui/react";
+
+import { SlideFade, useDisclosure } from "@chakra-ui/react";
+
 import { React, useState } from "react";
 import "./App.css";
-
+import { HamburgerIcon } from "@chakra-ui/icons";
 // -------------------------
 // List of main components
-
 import Header from "./Components/Header";
 import MainCalc from "./Components/MainCalc";
 // ----------------------------------------------
@@ -13,6 +24,7 @@ function App() {
     <div className="App">
       <Header />
       <main>
+        <Menu />
         <MainCalc />
       </main>
     </div>
@@ -20,3 +32,43 @@ function App() {
 }
 
 export default App;
+
+function Menu() {
+  // !!! Фишка по добавлению меню
+  // Уникальное состояние для chakra: свойство и метод
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <Box bg="white" position="sticky">
+      <HamburgerIcon
+        bg="blue.200"
+        onClick={onToggle}
+        w="45px"
+        h="45px"
+        p="5px"
+        m="5px"
+        borderRadius="5px"
+      />
+      <SlideFade
+        in={isOpen}
+        offsetY="-10px"
+        // отключение отображения HTML кода, когда меню скрыто, чтобы случайно не нажималось
+        style={isOpen ? { display: true } : { display: "none" }}
+      >
+        <Box
+          bg="blue.100"
+          border="1px solid white"
+          position="absolute"
+          zIndex="20"
+          w="40%"
+        >
+          <List display="flex" flexDirection="column" gap="3px" m="5px" p="8px">
+            <ListItem fontSize="18px">Calculator</ListItem>
+            <ListItem fontSize="18px">Converter</ListItem>
+            <ListItem fontSize="18px">Settings</ListItem>
+          </List>
+        </Box>
+      </SlideFade>
+    </Box>
+  );
+}
